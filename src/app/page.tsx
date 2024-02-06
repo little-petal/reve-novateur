86451f3fac8d97a017885229de0f645cd2304433
little-petal <julie.falque@protonmail.com>
2024-02-06 14:58:00 +0100

    add reel data and correctif

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       "status": "Terminé",
       "year": null,
       "type": [ "Economie", "MoE Etude et exécution", "Gros œuvre et TCE"],
-      "images": [""]
+      "images": ["1/20220328_154018.jpg"]
     },
     {
       "id": 2,
@@ -34,7 +34,7 @@ export default function Home() {
       "status": "Terminé",
       "year": null,
       "type": [ "Economie", "MoE Etude et exécution", "Gros œuvre et TCE"],
-      "images": ["20220309_100827.jpg"]
+      "images": ["2/20211130_095118.jpg"]
     },
     {
       "id": 3,
@@ -46,7 +46,7 @@ export default function Home() {
       "status": "Terminé",
       "year": null,
       "type": [ "Economie", "MoE Etude et exécution", "Lots - Démolition - terrassement - Gros oeuvre - Charpente"],
-      "images": ["20220328_154018.jpg"]
+      "images": ["3/20210902_173956.jpg"]
     },
     {
       "id": 4,
@@ -58,7 +58,7 @@ export default function Home() {
       "status": "Terminé",
       "year": null,
       "type": [ "Suivi - Terrassement et Dallage", "Réalisation Charpente - toiture - isolation paille", "Suivi - TCE"],
-      "images": ["71693510_2334398653487933_3944377817231261696_n.jpg"]
+      "images": ["4/20230804_143421.jpg"]
     },
     {
       "id": 5,
@@ -70,7 +70,7 @@ export default function Home() {
       "status": "Terminé",
       "year": null,
       "type": [ "Conception réalisation"],
-      "images": ["20220621_091014.jpg"]
+      "images": ["5/20220621_091014.jpg"]
     },
     {
       "id": 6,
@@ -82,7 +82,7 @@ export default function Home() {
       "status": "En cours",
       "year": null,
       "type": [ "AMO", "Economie", "MoE Etude et exécution - Réfection de l'intérieur"],
-      "images": [""]
+      "images": ["6/IMG-20230915-WA0008.jpg"]
     },
     {
       "id": 7,
@@ -94,7 +94,7 @@ export default function Home() {
       "status": "En cours",
       "year": null,
       "type": [ "AMO", "Economie", "MoE Etude et exécution  ITE + toiture"],
-      "images": [""]
+      "images": ["7/20240131_Facade Option 1_page-0003.jpg"]
     },
     {
       "id": 8,
@@ -106,7 +106,7 @@ export default function Home() {
       "status": "En cours",
       "year": null,
       "type": [ "Conception réalisation - Etanchéité de toiture terrasse + MeX + Corps d'états architecturaux"],
-      "images": ["20220403_094544.jpg"]
+      "images": ["8/FB_IMG_1646747313280.jpg"]
     }
   ] as Project[];
 
@@ -115,32 +115,38 @@ export default function Home() {
   const skills = [
     {
       "id": 1,
-      "name": "Compétence 1",
-      "description": "Description de la compétence 1",
+      "name": "Assistance Maîtrise d'ouvrage",
+      "description": "Nous travaillons pour conceptualiser votre projet et le structurer dans le cadre normatif actuel. Pour cela, nous avons le process pour vous permettre de questionner votre projet sur les plans financier, assurantiel, technique et esthétique. Étude de faisabilité du projet.<br />Définition du budget<br />Accompagnement pour les demandes d'aides au financement (Prim Rénov / Prim CEE / Financement privés)<br />Accompagnement dans les démarches administratives<br />Choix des solutions techniques",
       "icon": null
     },
     {
       "id": 2,
-      "name": "Compétence 2",
-      "description": "Description de la compétence 2",
+      "name": "Maîtrise d'oeuvre - CES / Isolation / Etanchéité",
+      "description": "Nous avons la compétence de réaliser les phases études de votre projet pour la réalisation des dossiers de consultation pour les lots des corps d'états secondaires.<br />Définition des solutions techniques en conformité avec les réglementations<br />Réalisation des dessins techniques des complexes et des raccords<br />Comptabilisation des quantitatifs ou des bordereaux<br />Consultation des entreprise<br />Analyse des offres<br />Contrat d'engagements<br />Direction des travaux",
       "icon": null
     },
     {
       "id": 3,
-      "name": "Compétence 3",
-      "description": "Description de la compétence 3",
+      "name": "Économie de la construction",
+      "description": "Nous réalisons l'ensemble de vos estimatifs de travaux afin de pouvoir avancer dans les phases études en vous aidant à garder le cap financier.<br />Définition des solutions techniques viables financièrement<br />Comptabilisation des quantitatifs ou des bordereaux<br />Contrôle de l'estimatif selon les variations du marché<br />Suivi des indices de la fédération française du bâtiment",
       "icon": null
     },
     {
       "id": 4,
-      "name": "Compétence 4",
-      "description": "Description de la compétence 4",
+      "name": "Maîtrise d'oeuvre en suivi d'exécution",
+      "description": "Nous coordonnons sur le secteur Haute Savoie / Savoie les entreprises de travaux. Nous garantissons ainsi la qualité, la propreté, la rapidité des travaux qui seront réalisés.<br />Compte rendu de chantier<br />Contrôle des réalisations conformément aux normes et aux CCTP<br />Suivi financiers et contrôle de facturation des entreprises<br />Réalisation des dossiers des ouvrages exécutés<br />Ordonnancement - Pilotage - Coordination",
       "icon": null
     },
     {
       "id": 5,
-      "name": "Compétence 5",
-      "description": "Description de la compétence 5",
+      "name": "Audit énergétique",
+      "description": "Pour vous accompagner dans votre projet de rénovation nous proposons un bilan énergétique de votre habitation et accompagnement dans les choix d'un programme de rénovation thermique.<br />Diagnostic de l'existant<br />Réalisation de l'audit énergétique<br />Réalisation du dossier de demande d'aides à la rénovation<br />Proposition d'axes d'améliorations selon des enveloppes budgétaires",
+      "icon": null
+    },
+    {
+      "id": 6,
+      "name": "Désamiantage",
+      "description": "Comme de nombreux projets de rénovation impliquent la complexité du désamiantage, nous avons la capacité d'encadrement technique amiante sous section 3.<br />Réalisation de plans de diagnostics amiante<br />Rédaction de plans de retraits d'amiante<br />Suivi des travaux de désamiantage/démolition",
       "icon": null
     }
   ] as Skill[];
@@ -175,21 +181,21 @@ export default function Home() {
         <div className="h-min lg:h-[90%] p6 relative">
           <div className="absolute bg-white/20 z-10 mt-6 sm:mt-12 lg:mt-16 p-2 md:p-6">
             <p className="font-bebas text-white text-3xl md:text-4xl lg:text-4xl xl:text-5xl p-2">Compétences</p>
-            <p className="font-inter text-white text-xl md:text-xl lg:text-xl xl:text-2xl p-2">Tout voir</p>
+            {/* <p className="font-inter text-white text-xl md:text-xl lg:text-xl xl:text-2xl p-2">Tout voir</p> */}
           </div>
-          <div className="lg:absolute inset-0 mt-0 sm:mt-16 lg:mt-0 grid grid-cols-1 grid-rows-2 sm:grid-cols-2 sm:grid-rows-2 lg:grid-cols-6 lg:grid-rows-5 p-6 gap-6">
-            <div className="w-full h-full row-span-1 sm:col-start-2 sm:col-span-1 lg:col-start-2 lg:col-span-2 lg:row-span-3 flex items-end">
+          <div className="2xl:absolute inset-0 mt-0 sm:mt-16 2xl:mt-0 grid grid-cols-1 grid-rows-2 sm:grid-cols-2 sm:grid-rows-2 lg:grid-cols-2 lg:grid-rows-2 2xl:grid-cols-6 2xl:grid-rows-5 p-6 gap-6">
+            <div className="w-full h-full row-span-1 sm:col-start-2 sm:col-span-1 lg:col-start-2 lg:col-span-1 lg:row-span-2 2xl:col-start-2 2xl:col-span-2 2xl:row-span-3 flex items-end">
               <List skills={skills} />
             </div>
-            <div className="w-full h-full row-span-1 row-start-3 lg:col-span-3 lg:row-span-5">
+            <div className="w-full h-full row-span-1 row-start-3 2xl:col-span-3 2xl:row-span-5">
               <img
                 src="/assets/images/skills.jpg"
                 alt="image"
-                className="hidden lg:block h-full w-full object-cover"
+                className="hidden 2xl:block h-full w-full object-cover"
               />
             </div>
-            <div className="bg-white/20 w-full h-full sm:col-span-2 lg:col-span-3 lg:row-span-2 flex items-center">
-              <p className="font-inter text-white text-xl md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl p-2 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis rhoncus nibh vitae volutpat. Nam condimentum a massa ut iaculis. Vivamus dictum leo non ipsum semper, ut luctus quam aliquet. Nullam eget suscipit mauris. In eu sodales nibh. Donec mattis eu leo id tempus. Quisque in mollis ex, at semper metus. Aenean sollicitudin mi non ex porttitor, vel tincidunt enim euismod. Quisque convallis porttitor quam, ut accumsan nulla. Quisque vel orci ac nisl maximus condimentum vitae at mauris. </p>
+            <div className="bg-white/20 w-full h-full sm:col-span-2 lg:start-col-3 lg:col-span-4 lg:row-span-1 2xl:col-span-3 2xl:row-span-2 flex items-center">
+              <p className="font-bellaza text-white text-xl md:text-2xl lg:text-xl xl:text-xl 2xl:text-xl p-2 text-center">Nous réalisons l&lsquo;encadrement et l&lsquo;organisation de vos projets de construction. Spécialisé dans la rénovation nous avons accumulé toutes les compétences afin de vous accompagner pour cadrer votre projet de réhabilitation depuis l&lsquo;audit énergétique jusqu&lsquo;à la restitution des clefs, en passant par le conseil en financements publiques. Comme toute maîtrise d&lsquo;œuvre nous savons travailler facilement sur des projets neufs. Comme certaines personnes souhaitent s&lsquo;investir dans les travaux de leur habitation nous cumulons des compétences transversales sur les constructions incluant des chantiers participatifs &#40;Neuf ossature bois isolation paille / Curage démantèlement / Enduits chaux, chaux chanvre / Jointement chaux ...&#41;. Cette capacité permet aux personnes motivées de réduire les coûts de leurs travaux.</p>
             </div>
           </div>
         </div>
@@ -202,7 +208,7 @@ export default function Home() {
         <div className="h-full lg:h-[90%] p6 relative">
           <div className="absolute bg-white/20 z-10 mt-6 sm:mt-12 lg:mt-16 p-2 md:p-6">
             <p className="font-bebas text-white text-3xl md:text-4xl lg:text-5xl p-2">Réalisations</p>
-            <p className="font-inter text-white text-xl md:text-xl lg:text-2xl p-2">Tout voir</p>
+            {/* <p className="font-inter text-white text-xl md:text-xl lg:text-2xl p-2">Tout voir</p> */}
           </div>
           <CarouselWithContent projects={projects}/>
         </div>
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -17,19 +17,20 @@ interface Props {
 
 export function List({skills}: Props) {
   return (
-      <Timeline className="lg:place-items-end xl:place-items-center">
+      <Timeline className="place-items-start">
         {skills.map((skill) => (
           <TimelineItem key={skill.id}>
             {(skill.id != skills.length) && <TimelineConnector />}
             <TimelineHeader className="h-3">
               <TimelineIcon />
-              <Typography variant="h3" color="blue-gray" className="font-bebas text-black" placeholder={undefined}>
+              <Typography variant="h3" color="blue-gray" className="font-bebas text-2xl md:text-3xl text-black" placeholder={undefined}>
                 {skill.name}
               </Typography>
             </TimelineHeader>
             <TimelineBody className="pb-8">
               <Typography color="blue" className="font-inter text-black" placeholder={undefined}>
-                {skill.description}
+                <br />
+                {/* {skill.description.slice(0, 10)} */}
               </Typography>
             </TimelineBody>
           </TimelineItem>
